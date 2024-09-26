9b0f6c5f5bef2a757a56e2f6b834d235da0a6539
sebastiananthony123 <tm@solumre.com>
2024-09-26 18:11:28 +0100

    operators

---
+++ operators.py
@@ -0,0 +1,20 @@
+x = 213
+y = 38
+
+sum = x + y
+sub = x-y
+mul = x * y
+div = x / y
+
+floorDivision = x//y 
+remainder = x % y
+expotent = x ** y
+
+print("Sum =" , sum)
+print("Diff =" , sub)
+print("Product =" , mul)
+print("Division =" , div)
+print("Floor Division =" , floorDivision)
+print("Remainder =" , remainder)
+print ("x to the power of y = " , expotent )
+
