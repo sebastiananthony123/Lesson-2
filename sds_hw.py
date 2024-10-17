1778c4b91403c465b3a0c0724207dec25d9231f8
sebastiananthony123 <tm@solumre.com>
2024-10-17 18:09:14 +0100

    sdshw

---
+++ sds_hw.py
@@ -0,0 +1,12 @@
+ # Take input values from user
+x = input("Enter Value of x:")
+y = input("Enter Value of y:")
+
+# Swapping
+a = x
+x = y
+y = a
+
+# Displaying results after swapping
+print("value of x after swapping", x)
+print("value of y after swapping", y)
