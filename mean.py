4f5b75fdca907d60bc0270871d2882cf8a2e5629
sebastiananthony123 <tm@solumre.com>
2024-10-17 17:43:07 +0100

    sds

---
+++ mean.py
@@ -0,0 +1,1 @@
+#The mean of 40 numbers is 38. Later on , we detected that we misread a number 56 as 36.Find the correct mean of the given numbers
---
+++ sds.py
@@ -0,0 +1,8 @@
+
+x = int(input("Enter a numerator: "))
+y = int(input("Enter a denominator: "))
+
+if x % y == 0:
+  print(f"{x} is divisible by {y}")
+else:
+   print(f"{x} is not divisible by {y}")
