7a1a7f6d8989df0fa560c52c8b7d1e87c1aa40e5
sebastiananthony123 <tm@solumre.com>
2024-10-17 17:52:14 +0100

    mean

--- mean.py
+++ mean.py
@@ -1,1 +1,18 @@
 #The mean of 40 numbers is 38. Later on , we detected that we misread a number 56 as 36.Find the correct mean of the given numbers
+
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+
+#sum of 40 numbers
+sum=mean1*total_number
+print("The sum of 40 numbers:",sum)
+
+#correct sum of these numbers
+SUM2=sum-(wrong_number)+(correct_number)
+print("CORRECTED SUM",SUM2)
+
+#the correct mean
+mean2=SUM2/total_number
+print(mean2)
