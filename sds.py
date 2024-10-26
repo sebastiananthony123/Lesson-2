bff674b1cf70d29e43196455fcc0a23ac385624e
sebastiananthony123 <tm@solumre.com>
2024-10-26 16:06:47 +0100

    Grade ten

--- sds.py
+++ sds.py
@@ -5,4 +5,4 @@ y = int(input("Enter a denominator: "))
 if x % y == 0:
   print(f"{x} is divisible by {y}")
 else:
-   print(f"{x} is not divisible by {y}")
+print(f"{x} is not divisible by {y}")
---
+++ ten-twentyHW.py
@@ -0,0 +1,6 @@
+x = int(input("Enter your age: "))
+
+if x < 20 and x > 10:
+    print(f"{x}You are allowed.")
+else:
+    print("You are not allowed")
