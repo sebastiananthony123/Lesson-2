8e0e4e10d1cc18bd36ddec67583d1abd61360374
sebastiananthony123 <tm@solumre.com>
2024-10-28 12:27:27 +0000

    loops

---
+++ loop.py
@@ -0,0 +1,22 @@
+for i in range(1,11,1):
+    print("loops")
+
+   
+
+
+
+for i in range(1,11,1):
+     #1 for increment is default
+    print(i)
+
+print("----------")
+
+#reversed loop
+for i in range(10 ,1 ,-1):
+    print(i)
+
+print("*****************")
+
+#odd numbers from 1-21
+for i in range(1,21,2):
+    print (i)
